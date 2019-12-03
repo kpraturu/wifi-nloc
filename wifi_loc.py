dd254bcba62153bdeb23bd4127ec024ad39ff8e9
Karthik Praturu <kart.prat@gmail.com>
2019-12-03 03:22:18 -0500

    Cleaned up plot generation code and added more commands for clearing plot and quitting simulation.

--- wifi_loc.py
+++ wifi_loc.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import heapq
 
+
+# Copied from stackoverflow solution to determining wifi data on OS X
+# source: https://stackoverflow.com/questions/15169022/is-there-any-way-to-access-os-x-wi-fi-data-using-python-signal-strength-for-e/15170342
 objc.loadBundle('CoreWLAN',
                 bundle_path='/System/Library/Frameworks/CoreWLAN.framework',
                 module_globals=globals())
@@ -23,6 +26,7 @@ def findWifi():
     RSSI:           %s""" % (iname, interface.ssid(), interface.transmitRate(),
                              interface.transmitPower(), interface.rssi())
     return interface
+
 
 def getDistance(interface):
     # Assumes measured dbm of -80 at 50 m away from AP
@@ -65,16 +69,32 @@ class DistanceEstimator:
             print "RSSI: " + str(rssi)
         return (distanceFromAP, rssi, self.curAvg)
 
+
+    def getDistanceSim(self, dev_loc, toPrint):
+        # sim_loc at (-8, -4)
+        sim_x = -6
+        sim_y = -4
+        (dev_x, dev_y) = dev_loc
+        dist = np.sqrt(np.abs(sim_x - dev_x)**2 + np.abs(sim_y - dev_y)**2)
+        sigma = dist*np.sqrt(2/np.pi)
+
+        distanceFromAP = np.random.rayleigh(sigma)
+
+        if toPrint:
+            print "Randomized Distance: " + str(distanceFromAP)
+            print "Actual Distance: " + str(dist)
+        return (distanceFromAP, dist, 0)
+
     def resetSamps(self):
         self.curAvg = 0
         self.num_samps = 0
+
 
 class Localizer:
     def __init__(self, numIter, sampSize):
         self.numIter = numIter
         self.x = 0
         self.y = 0
-        self.weight_list = []
         self.samp_size = sampSize
 
         grid_x = np.arange(-25, 25, 0.5)
@@ -132,6 +152,68 @@ class Localizer:
             plt.show()
             iterations += 1
 
+    def sim_localize(self):
+        ap_est = DistanceEstimator()
+        iterations = 0
+        while True:
+            input = raw_input("[" + str(iterations) + "] Input movement direction (wasd): ")
+
+            prev_x = self.x
+            prev_y = self.y
+
+            if input == 'w':
+                self.y += 1
+            elif input == 'a':
+                self.x -= 1
+            elif input == 's':
+                self.y -= 1
+            elif input == 'd':
+                self.x += 1
+            elif input == 'clear':
+                self.x = 0
+                self.y = 0
+
+                grid_x = np.arange(-25, 25, 0.5)
+                grid_y = np.arange(-25, 25, 0.5)
+
+                self.grid = []
+
+                for x in grid_x:
+                    for y in grid_y:
+                        # uniform prior
+                        self.grid.append((x, y, 1))
+                continue
+
+            elif input == 'q':
+                break
+
+            if not (prev_x == self.x and prev_y == self.y):
+                ap_est.resetSamps()
+
+            # Simulated AP at location -6, -4
+            distanceFromAP, _, _ = ap_est.getDistanceSim((self.x, self.y), True)
+
+            # Now do Bayesian analysis to determine MLE points
+            mle_locs = self.determineMLE(distanceFromAP, 10)
+
+            mle_x = []
+            mle_y = []
+            for x, y, prob in mle_locs:
+                mle_x.append(x)
+                mle_y.append(y)
+
+            print "My loc: " + str((self.x, self.y))
+            print "MLE AP loc: " + str(mle_locs[0])
+            plt.clf()
+            plt.scatter(mle_x, mle_y, color = 'blue')
+            plt.scatter(self.x, self.y, color = 'red')
+            plt.xlim(-10, 10)
+            plt.ylim(-10, 10)
+            plt.xlabel("meters")
+            plt.ylabel("meters")
+            plt.savefig("simiter.png")
+            plt.show(block=False)
+            iterations += 1
     # Assuming Rayleigh Distribution with std. dev of 1
     def determineMLE(self, distance, n):
         sigma = 1
@@ -189,10 +271,14 @@ plt.ylabel("Distance (m)")
 plt.show()
 
 plt.figure()
-plt.plot(range(10), regDists, estimatedDists)
+plt.plot(range(10), regDists, estimatedDists, ".-")
+plt.title("Measured Distance vs. Time")
+plt.xlabel("Sample Number")
+plt.ylabel("Measured Distance (m)")
+plt.legend(['Direct Calc', 'LSE'])
 plt.show()
 '''
 
 localizer = Localizer(10, 100)
 
-localizer.localize()
+localizer.sim_localize()
